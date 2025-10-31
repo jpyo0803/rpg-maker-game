390df101d5759d6602af1e31ef60fbf2f9b42aa7
Jinwon Pyo <gch99517@gmail.com>
2025-10-31 23:28:26 +0900

    Finish part 5 (scale 100 instead of 1)

---
+++ Assets/Tiled/TileSet A.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TileSet A" tilewidth="48" tileheight="48" tilecount="128" columns="8">
+ <image source="Forest_A5.png" width="384" height="768"/>
+</tileset>
